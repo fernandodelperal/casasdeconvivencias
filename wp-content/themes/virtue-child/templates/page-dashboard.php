c9ea8ef57dd3988d98011d6e1e981c45a7dba87f
fernandodelperal <31489588+fernandodelperal@users.noreply.github.com>
2024-11-26 10:40:28 -0300

    permissions

--- wp-content/themes/virtue-child/templates/page-dashboard.php
+++ wp-content/themes/virtue-child/templates/page-dashboard.php
@@ -34,8 +34,10 @@ get_header();
                 $sites = get_sites();
                 foreach ($sites as $site) {
                     $site_details = get_blog_details($site->blog_id);
-                    $selected = (get_current_blog_id() == $site->blog_id) ? 'selected' : '';
-                    echo '<option value="' . esc_url($site_details->siteurl) . '" ' . $selected . '>' . esc_html($site_details->blogname) . '</option>';
+                    if (current_user_can('manage_woocommerce', $site->blog_id)) {
+                        $selected = (get_current_blog_id() == $site->blog_id) ? 'selected' : '';
+                        echo '<option value="' . esc_url($site_details->siteurl) . '" ' . $selected . '>' . esc_html($site_details->blogname) . '</option>';
+                    }
                 }
                 ?>
             </select>
