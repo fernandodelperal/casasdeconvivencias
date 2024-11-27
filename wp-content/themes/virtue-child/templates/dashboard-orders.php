d12596c5709a010b91d94f385ca5f97353b54614
fernandodelperal <31489588+fernandodelperal@users.noreply.github.com>
2024-11-27 11:55:08 -0300

    200 limit

--- wp-content/themes/virtue-child/templates/dashboard-orders.php
+++ wp-content/themes/virtue-child/templates/dashboard-orders.php
@@ -73,7 +73,7 @@ if ( isset( $_GET['filter_product'] ) && ! empty( $_GET['filter_product'] ) ) {
 
 $orders = wc_get_orders( array(
     'status'   => 'any',
-    'limit'    => -1,
+    'limit'    => 200,
     'meta_query' => $meta_query,
 ) );
 
