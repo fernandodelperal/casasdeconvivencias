f4933b4b70b6452bb08dc19dd9f9b75d50a8b9cd
fernandodelperal <31489588+fernandodelperal@users.noreply.github.com>
2024-11-28 18:37:08 -0300

    Dejando de rastrear carpeta ignorada

--- wp-content/themes/virtue-child/templates/dashboard-settings.php
+++
@@ -1,5 +0,0 @@
-<h2>Configuraciones</h2>
-
-Administrar Medios de Pago:
-
-<a href="<?php echo site_url('/wp-admin/admin.php?page=wc-settings&tab=checkout'); ?>" class="btn btn-primary" style="margin-left: 10px; padding: 10px 20px; background-color: #0071a1; color: white; border-radius: 5px; text-decoration: none;">Formas de pago</a>
