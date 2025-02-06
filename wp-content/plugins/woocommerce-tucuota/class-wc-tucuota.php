864a73cd13f5d031a33f3549588d18c5a9279d47
fernandodelperal <31489588+fernandodelperal@users.noreply.github.com>
2025-02-06 18:24:57 -0300

    new combo

--- wp-content/plugins/woocommerce-tucuota/class-wc-tucuota.php
+++ wp-content/plugins/woocommerce-tucuota/class-wc-tucuota.php
@@ -503,7 +503,7 @@ class WC_TuCuota extends WC_Payment_Gateway
 
                         // actividades con plan de pago especial
                         //OJO PONER ID de las variaciones o si es producto simple, poner el id del producto. Por ej 1609 y 10 es combo elcasco varones 2021, indiv y compartida. 261 es test product
-                        if (in_array($product_id, [261, 1609, 1610, 2125, 2124, 5462, 3744])) {
+                        if (in_array($product_id, [261, 1609, 1610, 2125, 2124, 5462, 5496, 3744])) {
                             $special_interest = 0;
                             $special_installment = 12;
                             $final_amount = number_format($amount + $amount * $special_interest / 100, 2, ',', ' ');
